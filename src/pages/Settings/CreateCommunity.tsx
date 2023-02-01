789436fa9d790e7234e9f0243b081b2a9d91b8aa
0xprincedev <enthusiast0807@gmail.com>
2023-02-01 08:19:10 -0500

    add create community and manage communities

--- src/pages/Settings/CreateCommunity.tsx
+++ src/pages/Settings/CreateCommunity.tsx
@@ -1,11 +1,93 @@
+import { useState } from 'react'
+import { Field, Form } from 'react-final-form'
+import Button from '@mui/material/Button'
+
 import MainLayout from 'layouts/MainLayout'
+import InputForm from 'components/Form/InputForm'
+import ImageDropzone from 'components/ImageDropzone'
 
 const CreateCommunity = () => {
+	const [status, setStatus] = useState<number>(0)
+	const [file, setFile] = useState<any>(null)
+
+	const handleClick = () => {
+		setStatus(1)
+	}
+
+	const handleCreateCommunity = () => {}
+
 	return (
-		<MainLayout title="Raid Saas - Create Community">
+		<MainLayout title="Raid Saas - Create Community" className="create-community">
 			<div className="container">
 				<h1 className="title text-gradient">Create Community</h1>
-				<div className="content content-wrapper"></div>
+				<div className="content content-wrapper">
+					{status === 0 ? (
+						<div className="select-your-plan">
+							<h1 className="title text-gradient">
+								Lorem ipsum dolor sit amet, consectetur
+							</h1>
+							<p className="description">
+								Try every feature with a <b>30-day free trial</b>. No payment required.
+							</p>
+							<div className="plans">
+								<div className="plan">
+									<p className="plan__title">Monthly Plan</p>
+									<p className="plan__description">Pay as you go</p>
+									<div className="plan__detail">
+										<p>Every feature included</p>
+										<p>Set booking fees, sent instantly to your treasury</p>
+										<p>No hidden costs</p>
+									</div>
+									<div className="plan__divider" />
+									<p className="plan__highlight">
+										<b>0.59 SOL</b> for every 1K NFTs in your collection <b>per Month</b>
+									</p>
+									<Button className="btn-border-gradient">
+										<span className="btn__label" onClick={handleClick}>
+											Try for Free
+										</span>
+									</Button>
+								</div>
+								<div className="plan">
+									<p className="plan__title">Annual Plan</p>
+									<p className="plan__description">
+										Your whole community for one fixed price
+									</p>
+									<div className="plan__detail">
+										<p>Every feature offered, plus more</p>
+										<p>Set booking fees, sent instantly to your treasury</p>
+										<p>No hidden costs</p>
+									</div>
+									<div className="plan__divider" />
+									<p className="plan__highlight">
+										<b>Unlimited NFTs</b> in your collection <b>30 SOL</b> billed annually
+									</p>
+									<Button className="btn-border-gradient">
+										<span className="btn__label" onClick={handleClick}>
+											Try for Free
+										</span>
+									</Button>
+								</div>
+							</div>
+						</div>
+					) : (
+						<div className="settings">
+							<p className="settings__title">Community Settings</p>
+							<div className="settings__divider" />
+							<Form
+								onSubmit={handleCreateCommunity}
+								render={({ handleSubmit }) => (
+									<form onSubmit={handleSubmit}>
+										<Field name="name" label="Community Name">
+											{(props) => <InputForm {...props} />}
+										</Field>
+										<ImageDropzone defaultImage="/images/looties.png" setFile={setFile} />
+									</form>
+								)}
+							/>
+						</div>
+					)}
+				</div>
 			</div>
 		</MainLayout>
 	)
--- src/pages/Settings/Sections/ManageCommunities.tsx
+++ src/pages/Settings/Sections/ManageCommunities.tsx
@@ -17,62 +17,72 @@ const ManageCommunities = () => {
 				open={communityInfoEditModal}
 				closeModal={() => setCommunityInfoEditModal(false)}
 			/>
-			<div className="community-settings">
-				<h4>Community Settings</h4>
+			<div className="billing-due">
 				<div>
+					<p>Next Bill Due:</p>2/30/2023
+				</div>
+				<div>
+					<p>Total Due:</p>2.5 SOL
+				</div>
+			</div>
+			<div className="settings-group">
+				<div className="community-settings">
+					<h4>Community Settings</h4>
 					<div>
-						<img src="/images/looties.png" alt="" />
-						y00ts
-					</div>
-					<p>Twitter: @y00ts</p>
-					<p>Discord: /y00ts</p>
-					<Button onClick={handleEditCommunity}>Edit</Button>
-				</div>
-			</div>
-			<div className="collection-settings">
-				<h4>Collection Settings</h4>
-				<p className="description">
-					Members must hold at least 1 NFT from your linked collections
-				</p>
-				<div className="collections">
-					<div className="collection">
-						<p className="name">y00ts</p>
-						<p className="numbers">205</p>
-						<Button>Unlink</Button>
-					</div>
-					<div className="collection">
-						<p className="name">y00ts</p>
-						<p className="numbers">205</p>
-						<Button>Unlink</Button>
+						<div>
+							<img src="/images/looties.png" alt="" />
+							y00ts
+						</div>
+						<p>Twitter: @y00ts</p>
+						<p>Discord: /y00ts</p>
+						<Button onClick={handleEditCommunity}>Edit</Button>
 					</div>
 				</div>
-				<Button className="btn-border-gradient add-collection">
-					<span className="btn__label">Add Collection</span>
-				</Button>
-			</div>
-			<div className="booking-fees">
-				<h4>Booking Fees</h4>
-				<div className="bookings">
-					<div className="menu">
-						<p>Fee Treasury</p>
-						<p>Fee %</p>
-						<p>Total Fees Earned</p>
+				<div className="collection-settings">
+					<h4>Collection Settings</h4>
+					<p className="description">
+						Members must hold at least 1 NFT from your linked collections
+					</p>
+					<div className="collections">
+						<div className="collection">
+							<p className="name">y00ts</p>
+							<p className="numbers">205</p>
+							<Button>Unlink</Button>
+						</div>
+						<div className="collection">
+							<p className="name">y00ts</p>
+							<p className="numbers">205</p>
+							<Button>Unlink</Button>
+						</div>
 					</div>
-					<div className="booking">
-						<p className="address">
-							{shortenAddress('4FBqjSUBsYrkV2nSaSQ2fiythmSXRrQhXX3bN1A34M6R')}
-						</p>
-						<p className="fee">5%</p>
-						<p className="total-fees-earned">{(20000).toLocaleString()} SOL</p>
-						<Button>Edit</Button>
-					</div>
-					<div className="booking">
-						<p className="address">
-							{shortenAddress('4FBqjSUBsYrkV2nSaSQ2fiythmSXRrQhXX3bN1A34M6R')}
-						</p>
-						<p className="fee">5%</p>
-						<p className="total-fees-earned">{(20000).toLocaleString()} SOL</p>
-						<Button>Edit</Button>
+					<Button className="btn-border-gradient add-collection">
+						<span className="btn__label">Add Collection</span>
+					</Button>
+				</div>
+				<div className="booking-fees">
+					<h4>Booking Fees</h4>
+					<div className="bookings">
+						<div className="menu">
+							<p>Fee Treasury</p>
+							<p>Fee %</p>
+							<p>Total Fees Earned</p>
+						</div>
+						<div className="booking">
+							<p className="address">
+								{shortenAddress('4FBqjSUBsYrkV2nSaSQ2fiythmSXRrQhXX3bN1A34M6R')}
+							</p>
+							<p className="fee">5%</p>
+							<p className="total-fees-earned">{(20000).toLocaleString()} SOL</p>
+							<Button>Edit</Button>
+						</div>
+						<div className="booking">
+							<p className="address">
+								{shortenAddress('4FBqjSUBsYrkV2nSaSQ2fiythmSXRrQhXX3bN1A34M6R')}
+							</p>
+							<p className="fee">5%</p>
+							<p className="total-fees-earned">{(20000).toLocaleString()} SOL</p>
+							<Button>Edit</Button>
+						</div>
 					</div>
 				</div>
 			</div>
